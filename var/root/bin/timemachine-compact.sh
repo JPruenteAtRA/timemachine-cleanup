d0c144c3cb22179a1024e7e56e9909cbf6210d74
JPruenteAtRA <JPruenteAtRA@users.noreply.github.com>
2017-02-13 13:09:25 -0600

    Create timemachine-compact.sh
    /var/root/bin/timemachine-compact.sh
    root:wheel
    chmod 754

---
+++ var/root/bin/timemachine-compact.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Step through each backup sparsebundle and run an hdiutil compact on them
+# JPruente last modified 2017-02-10
+
+tmdir="/Volumes/Path-to-your-shared-TM-folder/Shared Items/Backups/"
+
+echo "$(ls -1 "${tmdir}" | grep -v "^\..*")" | while read i
+do
+    echo ${i}
+    if [ "$( hdiutil isencrypted "${tmdir}${i}" 2>&1 )" == "encrypted: NO" ]
+    then
+        hdiutil compact "${tmdir}${i}" 2>&1
+    else
+        echo "Backup is encrypted, skipping..."
+    fi
+done
