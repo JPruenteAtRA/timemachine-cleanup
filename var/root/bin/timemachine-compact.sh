effa6c531e10dc4b2f0f0ed217631c01ac8bc906
JPruenteAtRA <JPruenteAtRA@users.noreply.github.com>
2017-02-14 14:26:03 -0600

    Enhance how messages are handled and reported
    Don't assume anything other than "encrypted: NO" means it failed due will be due to encryption. Echo the output of the failure so it gets caught in the main log.

--- var/root/bin/timemachine-compact.sh
+++ var/root/bin/timemachine-compact.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 #
 # Step through each backup sparsebundle and run an hdiutil compact on them
-# JPruente last modified 2017-02-10
+# JPruente last modified 2017-02-14
 
 tmdir="/Volumes/Path-To-Your-TM-Share/Shared Items/Backups/"
 
 echo "$(ls -1 "${tmdir}" | grep -v "^\..*")" | while read i
 do
     echo ${i}
-    if [ "$( hdiutil isencrypted "${tmdir}${i}" 2>&1 )" == "encrypted: NO" ]
+    isencout="$( hdiutil isencrypted "${tmdir}${i}" 2>&1 | tail -n 1 )"
+    if [ "${isencout}" == "encrypted: NO" ]
     then
         hdiutil compact "${tmdir}${i}" 2>&1
     else
-        echo "Backup is encrypted, skipping..."
+        echo "Skipping: ${isencout}"
     fi
 done
